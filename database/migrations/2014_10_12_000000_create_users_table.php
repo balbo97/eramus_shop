4769f0d02843b3fea93fd9dc8324b84cfb2cab4b
balbo97 <balbosauro1@gmail.com>
2024-04-29 21:42:25 +0200

    Add surname and birthdate fields to User model and migration; update package-lock.json
    The following changes were made:
    
    * The `User` model and migration were updated to add `surname` and `birth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname'); // Aggiungi la colonna per il cognome
+            $table->date('birthdate'); // Aggiungi la colonna per la data di nascita
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
